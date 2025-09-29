78227d9e208f54bd98c3051ce025ce79a5be68ca
B00971548 <B00971548@MF124PC27>
2025-09-29 15:56:22 +0100

    Updated tasks

--- COM326Week1/Program.cs
+++ COM326Week1/Program.cs
@@ -1,5 +1,5 @@
 ﻿
-//task 1
+//task 1 - processing user input
 Console.WriteLine("Please enter your name: ");
 String name = Console.ReadLine();
 
@@ -22,20 +22,20 @@ else
 {
     Console.WriteLine("This is your age");
 }
+Console.WriteLine();
 
 
 
-
-//task 2
+//task 2 - selection statements
 if (age < 13)
 {
     Console.WriteLine("you are a child");
 }
-else if (age >= 13 | age <=19)
+else if (age >= 13 && age <=19)
 {
     Console.WriteLine("you are a teenager");
 }
-else if (age > 19 | age <= 110)
+else if (age > 19 && age <= 110)
 {
     Console.WriteLine("you are an adult");
 }
@@ -43,5 +43,65 @@ else
 {
     Console.WriteLine("You are out of the age range");
 }
+Console.WriteLine();
 
 
+
+
+//task 3 - logical operators
+Console.WriteLine("Please enter an integer");
+int i = Convert.ToInt32(Console.ReadLine());
+if (i == 0)
+{
+    Console.WriteLine("This number is zero");
+}
+else if (i > 0)
+{
+    Console.WriteLine("This is a positive number");
+}
+else
+{
+    Console.WriteLine("This is a negative number");
+}
+Console.WriteLine();
+
+
+
+
+
+//task 4 - Leap Year Checker
+Console.WriteLine("Please enter a year to check if it is a leap year");
+int year = Convert.ToInt32(Console.ReadLine());
+int r = year % 4;
+if (year < 0)
+{
+    Console.WriteLine("This is not a valid year");
+}
+else if (r == 0)
+{
+    Console.WriteLine(year + " is a leap year");
+}
+else
+{
+    Console.WriteLine(year + " is not a leap year");
+}
+Console.WriteLine();
+
+
+
+
+
+
+
+//task 5 - even or odd checker
+Console.WriteLine("Please enter an integer to check for even and odd");
+int number = Convert.ToInt32(Console.ReadLine());
+int a = number % 2;
+if (r == 0)
+{
+    Console.WriteLine("This is an even number");
+}
+else
+{
+    Console.WriteLine("This is an odd number");
+}
